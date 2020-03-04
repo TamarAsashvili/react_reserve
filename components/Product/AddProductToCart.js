e44811b410dd12aae8f1371fb0b71227b33054cd
Tamar <asashvili@gmail.com>
2020-03-04 22:58:46 +0100

    add to cart product

--- components/Product/AddProductToCart.js
+++ components/Product/AddProductToCart.js
@@ -1,5 +1,10 @@
+import { Input } from 'semantic-ui-react'
+
 function AddProductToCart() {
-  return <>AddProductToCart</>;
+  return (
+    <Input value={1} type='number' min='1' placeholder='Quantity'
+      action={{ color: 'orange', content: 'Add To Cart', icon: 'plus cart' }} />
+  )
 }
 
 export default AddProductToCart;
