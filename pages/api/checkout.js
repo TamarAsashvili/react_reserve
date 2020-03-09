0853947a7f731c1d1b3381979407345e21e13905
Tamar <asashvili@gmail.com>
2020-03-09 13:54:47 +0100

    create order

--- pages/api/checkout.js
+++ pages/api/checkout.js
@@ -2,7 +2,9 @@ import Stripe from 'stripe'
 import uuidv4 from 'uuid/v4'
 import jwt from 'jsonwebtoken'
 import Cart from '../../models/Cart'
-import calculateCartTotal from '../../utils/calculateCartTotal'
+import Order from '../../models/Order'
+import calculateCartTotal from '../../utils/calculateCartTotal';
+
 
 const stripe = Stripe(process.env.STRIPE_SECRET_KEY)
 
